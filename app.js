eed5555ee722d7a3966f9f1afc3f374690343ae7
Ryuunofire27 <cdvillagomez27@gmail.com>
2018-02-18 15:33:15 -0500

    v1.0.0

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const publicDir = express.static(`${__dirname}/dist`);
 app.use(publicDir);
 
 app.get('/', (req,res,next) => {
-  res.render(`${estatico}/index.html`);
+  res.render(`index.html`);
 });
 
 app.listen(8000);
